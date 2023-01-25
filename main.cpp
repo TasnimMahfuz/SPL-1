f4851cde7341b801f36ea62f7edbd040c2a31189
Tasnim Mahfuz Nafis <81380096+TasnimMahfuz@users.noreply.github.com>
2023-01-25 09:22:09 +0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  cout<<"I am now learning the implementation of AES algorithm and socket programming.\n";
+  cout<<"I can start writing project codes very soon."<<endl;
+  
+  return 0;
+}
